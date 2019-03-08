48f9a6ce6f2de841a5e3f164d78a3ed053548a9a
Stephen Brewer <stephenbrewer16@gmail.com>
2019-03-08 01:33:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ var prependKitten = function(){
   return kittens
 }
 var removeLastKitten = function(){
-  return kittens.slice[0, kittens.length - 1]
+  return kittens.slice(0, kittens.length - 1)
 }
